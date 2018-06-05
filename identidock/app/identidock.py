f3e3fc665ac8003b3bd6e9d7f6d7f77265f430be
Test <test@test.com>
2018-06-05 17:54:58 +0300

    image

--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -1,9 +1,30 @@
-from flask import Flask
+from flask import Flask, Response
+import requests
+
 app = Flask(__name__)
+def_name = "Horse"
 
 @app.route('/')
-def hello_world():
-        return 'Hello people\n'
+def mainpage():
+	name = def_name
+
+	header = '<html><head><title>Hello people</title></head><body>'
+	body = '''<form method="Post">
+		  Hello <input type="text" name="name" value="{}">
+		  <input type="submit" value="Enter">
+		  </form>
+		  <p>You look like a:
+		  <img src="/monster/monster.png">
+		  '''.format(name)
+	footer = '</body></html>'
+	return header + body + footer
+
+@app.route('/monster/<name>')
+def get_identicon(name):
+	req = requests.get('http://dmonster:8000/monster/' + name + '?size=80')
+	image = req.content
+
+	return Response(image, mimetype='image/png')
 
 if __name__ == '__main__':
         app.run(debug=True, host='0.0.0.0')
