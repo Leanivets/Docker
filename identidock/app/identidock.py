1dc47bf412d84d1ab88b1865d876bf8af17b26e9
Test <test@test.com>
2018-06-06 13:17:50 +0300

    Dyn png with cache

--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -1,8 +1,10 @@
 from flask import Flask, Response, request
 import requests
 import hashlib
+import redis
 
 app = Flask(__name__)
+cache = redis.StrictRedis(host='redis', port=6379, db=0)
 salt = "UNIQUE_SALT"
 def_name = 'Horse'
 
@@ -30,8 +32,11 @@ def mainpage():
 
 @app.route('/monster/<name>')
 def get_identicon(name):
-	req = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
-	image = req.content
+	image = cache.get(name)
+	if image is None:
+		req = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
+		image = req.content
+		cache.set(name, image)
 
 	return Response(image, mimetype='image/png')
 
