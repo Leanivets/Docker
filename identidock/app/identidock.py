c5c46564c998db990475aa2e15abc27310958bc5
Test <test@test.com>
2018-06-06 12:34:11 +0300

    Dyn png

--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -1,21 +1,30 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 import requests
+import hashlib
 
 app = Flask(__name__)
-def_name = "Horse"
+salt = "UNIQUE_SALT"
+def_name = 'Horse'
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def mainpage():
 	name = def_name
 
-	header = '<html><head><title>Hello people</title></head><body>'
-	body = '''<form method="Post">
-		  Hello <input type="text" name="name" value="{}">
-		  <input type="submit" value="Enter">
+	if request.method == 'POST':
+        	name = request.form['name']
+
+	salted_name = salt + name
+	name_hash = hashlib.sha256(salted_name.encode()).hexdigest()
+
+	header = '<html><head><title>Identidock</title></head><body>'
+	body = '''<form method="POST">
+		  Hello <input type="text" name="name" value="{0}">
+		  <input type="submit" value="submit">
 		  </form>
 		  <p>You look like a:
-		  <img src="/monster/monster.png"/>
-		  '''.format(name)
+		  <img src="/monster/{1}"/>
+		  '''.format(name, name_hash)
 	footer = '</body></html>'
 	return header + body + footer
 
@@ -27,4 +36,4 @@ def get_identicon(name):
 	return Response(image, mimetype='image/png')
 
 if __name__ == '__main__':
-        app.run(debug=True, host='0.0.0.0')
+	app.run(debug=True, host='0.0.0.0')
